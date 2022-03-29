4252de373bfbb7261591afd5d4fcaa1453149972
Pincon <xavierpincon@gmail.com>
2022-03-29 15:18:24 +0200

    scss

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-
 import Accueil from './pages/Accueil';
 import Contact from './pages/Contact';
 import Exercices from './pages/Exercices';
 import Parametres from './pages/Parametres';
 import Profil from './pages/Profil';
 import Programme from './pages/Programme';
+
+// Recuperation des données de l'application symfony sans passé par heroku (pour l'instant)!!
+// async componentDidMount(){
+//   const response = await fetch ("http://127.0.0.1:8000/main");
+//   const datas = await response.json();
+// }
 
 
 const App = ()=> {
@@ -21,7 +26,6 @@ const App = ()=> {
     <Route path="/profil" element={<Profil />} />
     <Route path="/contact" element={<Contact/>} />
     <Route path="/parametres" element={<Parametres />} />
-
   </Routes>
   </BrowserRouter>
     
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import{ NavLink} from "react-router-dom";
+import "../styles/components/navigation.css";
 
 const Navigation = () => {
     return (
-        <div>
+        <nav>
+            <div className='logo'><NavLink to="/accueil"> <li><img src="img/logo.png" alt="" /></li> </NavLink></div>
+        <div className='navbar'>
             <ul>
-            <NavLink to="/accueil"> <li><img src="../assets/img/logo.png" alt="" /></li> </NavLink>
-            <NavLink to="/accueil"> <li><img src="../assets/img/Home.png" alt="" /></li> </NavLink>
-            <NavLink to="/programme"> <li><img src="../assets/img/programme.png" alt="" /></li> </NavLink>
-            <NavLink to="/exercices"> <li><img src="../assets/img/exercice.png" alt="" /></li> </NavLink>
-            <NavLink to="/profil"> <li><img src="../assets/img/profil.png" alt="" /></li> </NavLink>
-            <NavLink to="/contact"> <li><img src="../assets/img/contact.png" alt="" /></li> </NavLink>
-            <NavLink to="/contact"> <li><img src="../assets/img/parametre.png" alt="" /></li> </NavLink>
-            <NavLink to="/accueil"> <li><img src="../assets/img/deconnexion.png" alt="" /></li> </NavLink>
-               
+            <NavLink to="/accueil"> <li><img src="img/Home.png" alt="" /></li> </NavLink>
+            <NavLink to="/programme"> <li><img src="img/programme.png" alt="" /></li> </NavLink>
+            <NavLink to="/exercices"> <li><img src="img/exercice.png" alt="" /></li> </NavLink>
+            <NavLink to="/profil"> <li><img src="img/profil.png" alt="" /></li> </NavLink>
+            <NavLink to="/contact"> <li><img src="img/contact.png" alt="" /></li> </NavLink>
+            <NavLink to="/contact"> <li><img src="img/parametre.png" alt="" /></li> </NavLink>
+            <NavLink to="/accueil"> <li><img src="img/deconnexion.png" alt="" /></li> </NavLink>    
             </ul>
         </div>
+        </nav>
     );
 };
 
